be61807becdf9f70d4063ee32cb0085836072a9a
lishuai <lishuai@northcpt.com>
2024-12-27 17:49:34 +0800

    1

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.tsx'
 import Header from './components/common/Header.tsx'
 import Footer from './components/common/Footer.tsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
